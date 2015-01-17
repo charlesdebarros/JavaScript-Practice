683000969ebbc4d3b99a87349d011c34e00dae41
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-01-17 17:56:50 +0000

    Making script print specific info for all or none trains running.

--- scripts/trains.js
+++ scripts/trains.js
@@ -1,16 +1,27 @@
 var maximum = 20, minimum = 1;
 var randomNumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+var totalTrains = 20;
 var trainsOperational = randomNumber ;
 var lateDeparture = trainNumber == randomNumber
-var totalTrains = 20;
 
 
-for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
-    if (trainNumber <= trainsOperational) {
-        document.write("Train # " + trainNumber + " is running." + "<br>");
-    } else if (trainNumber == 8 || trainNumber == 17) {
-        document.write("Train # " + trainNumber + " begins running at noon." + "<br>");
+
+if (trainsOperational > 0) {
+    if (trainsOperational == totalTrains){
+        document.write("All trains are operational at this time. Have a great day!" + "<br>");
     } else {
-        document.write("Train # " + trainNumber + " is not operational." + "<br>");
+        for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
+            if (trainNumber <= trainsOperational) {
+                document.write("Train # " + trainNumber + " is running." + "<br>");
+            } else if (trainNumber == 8 || trainNumber == 17) {
+                document.write("Train # " + trainNumber + " begins running at noon." + "<br>");
+            } else {
+                document.write("Train # " + trainNumber + " is not operational." + "<br>");
+            }
+        }
     }
+} else {
+    document.write("No trains are operation at the moment. Apologies for that." + "<br>");
 };
+
+
