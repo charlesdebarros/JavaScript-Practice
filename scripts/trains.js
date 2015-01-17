3bd65ba5003b352278f63b3da8219e4a40e2e570
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-01-17 19:05:34 +0000

    Making train 3 only run on Sundays. Display info about train 3 and styling the date format.

--- scripts/trains.js
+++ scripts/trains.js
@@ -2,19 +2,19 @@ var maximum = 20, minimum = 1;
 var randomNumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 var totalTrains = 20;
 var trainsOperational = randomNumber ;
-var lateDeparture = trainNumber == randomNumber
-
-
+var dayOfWeek = new Date(); 
 
 if (trainsOperational > 0) {
     if (trainsOperational == totalTrains){
         document.write("All trains are operational at this time. Have a great day!" + "<br>");
     } else {
         for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
-            if (trainNumber <= trainsOperational) {
+            if (trainNumber <= trainsOperational && trainNumber != 3) {
                 document.write("Train # " + trainNumber + " is running." + "<br>");
             } else if (trainNumber == 8 || trainNumber == 17) {
                 document.write("Train # " + trainNumber + " begins running at noon." + "<br>");
+            } else if (trainNumber == 3 && dayOfWeek.getDate() != 0) {
+                document.write("Train # 3 only runs on Sundays." + "<br>");
             } else {
                 document.write("Train # " + trainNumber + " is not operational." + "<br>");
             }
@@ -24,4 +24,4 @@ if (trainsOperational > 0) {
     document.write("No trains are operation at the moment. Apologies for that." + "<br>");
 };
 
-
+document.write("<br>Today is " + dayOfWeek.toDateString() + ".");
