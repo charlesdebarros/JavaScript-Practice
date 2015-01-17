0728bcbff029b0e3d1237726aae82e2911b4b3e6
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-01-17 16:46:13 +0000

    Adding 'train starts at noon' feature.

--- scripts/trains.js
+++ scripts/trains.js
@@ -1,13 +1,16 @@
 var maximum = 20, minimum = 1;
 var randomNumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 var trainsOperational = randomNumber ;
+var lateDeparture = trainNumber == randomNumber
 var totalTrains = 20;
 
 
 for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
     if (trainNumber <= trainsOperational) {
-    document.write("Train # " + trainNumber + " is running." + "<br>");
+        document.write("Train # " + trainNumber + " is running." + "<br>");
+    } else if (trainNumber == 8 || trainNumber == 17) {
+        document.write("Train # " + trainNumber + " begins running at noon." + "<br>");
     } else {
-    document.write("Train # " + trainNumber + " is not operational." + "<br>");
+        document.write("Train # " + trainNumber + " is not operational." + "<br>");
     }
 };
