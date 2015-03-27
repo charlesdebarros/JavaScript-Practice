34dbe30b217cc408dc331022e3de75a51d949da2
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-03-27 22:49:12 +0000

    Expanding trains.js and hoover_dam.js files.

--- scripts/hoover_dam.js
+++ scripts/hoover_dam.js
@@ -2,14 +2,31 @@ var currentGen = 1;
 var totalGen = 19;
 var totalMW = 0;
 
-while (currentGen <= 4){
-	totalMW += 62;
-  console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
-	currentGen ++;
+var totalGen = 19;
+var totalMW = 0;
+
+for (var currentGen = 1; currentGen <= totalGen; currentGen++){
+  if (currentGen % 2 == 0){
+    if (currentGen <= 4){
+      totalMW += 62;
+    	console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
+    } else if ( currentGen <= totalGen){
+      totalMW += 124;
+    	console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
+    };
+  } else {
+    	console.log("Generator #" + currentGen + " is off.");
+  };
 };
 
-for (currentGen = 5; currentGen <= totalGen; currentGen++ ){
-	totalMW += 124;
-  console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
+// while (currentGen <= 4){
+// 	totalMW += 62;
+//   console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
+// 	currentGen ++;
+// };
 
-};
+// for (currentGen = 5; currentGen <= totalGen; currentGen++ ){
+// 	totalMW += 124;
+//   console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
+
+// };
--- scripts/trains.js
+++ scripts/trains.js
@@ -17,7 +17,7 @@ document.write("<h2>" + greeting + "</h2>");
 var maximum = 20, minimum = 1;
 var randomNumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 var totalTrains = 20;
-var trainsOperational = randomNumber ;
+var trainsOperational = randomNumber;
 var dayOfWeek = new Date(); 
 
 document.write("<h2>" + "Today's trains situation at the moment: " + "</h2>")
@@ -27,9 +27,9 @@ if (trainsOperational > 0) {
         document.write("All trains are operational at this time. Have a great day!" + "<br>");
     } else {
         for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
-            if (trainNumber <= trainsOperational && trainNumber != 3 && trainNumber != 17) {
+            if (trainNumber <= trainsOperational && trainNumber != 3) {
                 document.write("Train # " + trainNumber + " is running." + "<br>");
-            } else if (hourNow == 12 && trainNumber == 8 || trainNumber == 17) {
+            } else if (hourNow == 23 && trainNumber == 8 || trainNumber == 17) {
                 document.write("Train # " + trainNumber + " begins running at noon." + "<br>");
             } else if (trainNumber == 3 && dayOfWeek.getDate() != 0) {
                 document.write("Train # 3 only runs on Sundays." + "<br>");
@@ -42,4 +42,4 @@ if (trainsOperational > 0) {
     document.write("No trains are operation at the moment. Apologies for that." + "<br>");
 };
 
-document.write("<br>Today is " + dayOfWeek.toDateString() + ".");
+document.write("<br>Today is " + dayOfWeek.toDateString() + "," + today.getTime() + ".");
