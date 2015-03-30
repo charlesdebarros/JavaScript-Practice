6d700f21775ccb041bb3fb8c09abd4515cf26fb8
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-03-30 21:49:28 +0100

    Adding immediatelyInvokedPuzzler script to complete part 3, level 1

---
+++ scripts/immediatelyInvokedPuzzler.js
@@ -0,0 +1,8 @@
+var puzzlers = [
+  function(a) { return 8 * a - 10; },
+  function(a) { return (a - 3) * (a - 3) * (a - 3); },
+  function(a) { return a * a + 4; },
+  function(a) { return a % 5; }
+];
+
+alert(puzzler[puzzler[1](3)](puzzler[3](9)););
