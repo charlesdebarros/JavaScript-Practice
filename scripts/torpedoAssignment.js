c7b7288e07aa0e7d80447db7d9aee74d8fbccdad
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-12-07 18:29:38 +0000

    Adding torpedoAssignment.js script

---
+++ scripts/torpedoAssignment.js
@@ -0,0 +1,13 @@
+function assignTorpedo(name, passengerArray){
+  for (var i = 0; i < passengerArray.length; i++){
+    if(passengerArray[i] == name) {
+      return function(){
+        alert("Ahoy, " + name + "!\n" + "Man your post at Torpedo #" + (i+1) + "!");
+      };
+    }
+  }
+};
+
+var subPassengers = ["Luke", "Leia", "Han", "Chewie", "Yoda", "R2-D2", "C-3PO", "Boba"];
+var giveAssignment = assignTorpedo("Chewie", subPassengers);
+giveAssignment();
